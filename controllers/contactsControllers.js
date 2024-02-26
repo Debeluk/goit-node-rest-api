9b148a1a32f0c9792a9bf0d5b579425c4b31a2ac
Debeluk <popcorn.starinets@gmail.com>
2024-02-26 17:28:09 +0200

    2.1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,60 @@
-import contactsService from "../services/contactsServices.js";
+import { listContacts, getContactById, removeContact, addContact, updateContact } from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.json(contacts);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(Number(id));
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(contact);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await removeContact(Number(id));
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(contact);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await addContact(name, email, phone);
+    res.status(201).json(newContact);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(Number(id));
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    const updatedContact = await contactsService.updateContact(Number(id), req.body);
+    res.json(updatedContact);
+  } catch (err) {
+    next(err);
+  }
+};
